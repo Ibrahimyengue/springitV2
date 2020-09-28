4dac0354710167084414bb68fa1c950bb905add0
Ibrahimyengue <ibrahimyengue@gmail.com>
2020-09-28 12:24:02 -0400

    Initial Commit for our Sprint Boot Redit Clone!

---
+++ src/test/java/com/ibrahimyengue/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimyengue.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
